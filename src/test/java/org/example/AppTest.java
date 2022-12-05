9bb36d0689a64dc7362ae253d7e0b0168c155f51
MaximGromak <maximgromak@gmail.com>
2022-12-06 02:04:21 +1000

    browserup-proxy not worked

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,11 +1,32 @@
 package org.example;
 
 
+import com.browserup.harreader.model.Har;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+
+import java.io.File;
+import java.time.Duration;
 
 public class AppTest extends BaseTest{
     @Test
+    @SneakyThrows
     public void shouldAnswerWithTrue() {
+        proxy.newHar("demoqa.com");
+        driver.get("https://demoqa.com/text-box");
 
+        driver.findElement(By.id("userName")).sendKeys("qwerty_name");
+        driver.findElement(By.id("userEmail")).sendKeys("qwe@mail.ru");
+        driver.findElement(By.id("currentAddress")).sendKeys("qa_qa_qa");
+        driver.findElement(By.id("permanentAddress")).sendKeys("aaaaaaaaaaaaddddddddddddddreeeeeeeeeeeessssss");
+
+        driver.findElement(By.id("submit")).click();
+        new Actions(driver).pause(Duration.ofSeconds(2)).perform();
+
+        Har har = proxy.getHar();
+        File harFile = new File("demoqa.com");
+        har.writeTo(harFile);
     }
 }
--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,19 +1,57 @@
 package org.example;
 
+import com.browserup.bup.BrowserUpProxyServer;
+import com.browserup.bup.client.ClientUtil;
+import com.browserup.bup.proxy.CaptureType;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.Inet4Address;
+import java.time.Duration;
 
 public class BaseTest {
-    public ChromeDriver driver;
+    public WebDriver driver;
+    public BrowserUpProxyServer proxy;
+    public Proxy seleniumProxy;
 
+    @SneakyThrows
     @BeforeEach
     void setUp() {
+        proxy = new BrowserUpProxyServer();
+        proxy.start();
+
+        seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+        proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
+
+        DesiredCapabilities seleniumCapabilities = new DesiredCapabilities();
+        seleniumCapabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(                             "--window-size=1920,1080");
+        options.merge(seleniumCapabilities);
+
+        WebDriverManager.chromedriver()
+                .capabilities(seleniumCapabilities)
+                .setup();
+
+        this.driver = new ChromeDriver(options);
+        this.driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
 
     }
 
     @AfterEach
     void tearDown() {
-
+        proxy.stop();
+        if (driver != null){
+            driver.quit();
+        }
     }
 }
